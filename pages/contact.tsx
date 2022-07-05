0b3c7e84561604da6f673a31224ff8fbc0acf4ac
Arzivre <sonyfauzi@outlook.co.id>
2022-07-05 20:42:11 +0700

    ui: refactor contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,36 +2,43 @@ import { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import Layout from 'components/Layout'
 
+type formData = {
+  user_email: string
+  user_name: string
+  message: string
+}
+
+const leftSection = (
+  <section>
+    <h1 className='text-[calc(1em+5vh)] mb-8'>
+      Let&apos;s Build Something Together
+    </h1>
+    <p className='text-xl'>sonyfauzi@outlook.co.id</p>
+    <p className='text-xl'>0813 3205 6357</p>
+    <p className='text-xl mb-8'>Akan menjawab dalam 24 jam.</p>
+    <p className='text-xl'>Kediri, Jawa Timur.</p>
+  </section>
+)
+
 export default function ContactUs() {
-  const [data, setData] = useState({
+  const [submit, setSubmit] = useState<boolean>(false)
+  const [data, setData] = useState<formData>({
     user_email: '',
     user_name: '',
     message: '',
   })
 
-  const form = useRef()
-
-  const textareaStyle = ''
-
-  const left = (
-    <section>
-      <h1 className=''>Let&apos;s Build Something Together</h1>
-      <p className=''>sonyfauzi@outlook.co.id</p>
-      <p className=''>0813 3205 6357</p>
-      <p className=''>akan menjawab dalam 24 jam.</p>
-      <p>Kediri, Jawa Timur.</p>
-    </section>
-  )
+  const formRef = useRef<formData>()
 
   const sendEmail = (e: { preventDefault: () => void }) => {
     e.preventDefault()
 
     emailjs
       .sendForm(
-        'service_4t7ll1q',
-        'template_jgzo41g',
-        form.current as any,
-        'XO3SeKnaksOzfRKEA'
+        process.env.NEXT_PUBLIC_SERVICE as string,
+        process.env.NEXT_PUBLIC_TEMPLATE as string,
+        formRef.current as any,
+        process.env.NEXT_PUBLIC_KEY as string
       )
       .then(
         (result) => {
@@ -41,42 +48,65 @@ export default function ContactUs() {
           console.log('FAILED...', error.text)
         }
       )
+    setSubmit(true)
   }
 
   return (
     <Layout>
-      <main className='container mx-auto px-4 grid grid-cols-2 my-20'>
-        {left}
-        <form ref={form as any} onSubmit={sendEmail}>
-          <input
-            placeholder='your@email.com'
-            name='user_email'
-            required
-            value={data.user_email}
-            onChange={(e) => setData({ ...data, user_email: e.target.value })}
-          />
-          <input
-            placeholder='John Doe'
-            name='user_name'
-            value={data.user_name}
-            onChange={(e) => setData({ ...data, user_name: e.target.value })}
-          />
-          <textarea
-            required
-            placeholder='I want to say hi!'
-            name='message'
-            rows={4}
-            value={data.message}
-            onChange={(e) => setData({ ...data, message: e.target.value })}
-          />
+      <main className='container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 space-y-8 my-20'>
+        {leftSection}
+        <form
+          ref={formRef as any}
+          onSubmit={sendEmail}
+          className='p-10 bg-blue-600/10 rounded-md'
+        >
+          {!submit ? (
+            <>
+              <input
+                placeholder='your@email.com'
+                name='user_email'
+                required
+                value={data.user_email}
+                onChange={(e) =>
+                  setData({ ...data, user_email: e.target.value })
+                }
+                className='w-full mb-4 form-input px-4 py-3 rounded-md'
+              />
 
-          <button
-            className='px-4 py-2 m-4 bg-blue-400 '
-            type='submit'
-            value='Send'
-          >
-            Kirim Pesan
-          </button>
+              <input
+                placeholder='John Doe'
+                name='user_name'
+                value={data.user_name}
+                onChange={(e) =>
+                  setData({ ...data, user_name: e.target.value })
+                }
+                className='w-full mb-4 form-input px-4 py-3 rounded-md'
+              />
+              <textarea
+                required
+                placeholder='I want to say hi!'
+                name='message'
+                rows={4}
+                value={data.message}
+                onChange={(e) => setData({ ...data, message: e.target.value })}
+                className='w-full mb-4 form-textarea px-4 py-3 rounded-md'
+              />
+
+              <button
+                className='px-4 py-2 my-4 bg-blue-600 text-blue-50 rounded-md 
+                hover:text-blue-900 hover:bg-blue-300'
+                type='submit'
+                value='Send'
+                disabled={submit}
+              >
+                Kirim Pesan
+              </button>
+            </>
+          ) : (
+            <h1 className='my-auto text-[calc(1em+5vh)] text-center '>
+              Pesan Terkirim
+            </h1>
+          )}
         </form>
       </main>
     </Layout>
