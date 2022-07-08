9ca2f29a0a4400845874bdba82a67d44bc42c2f6
Arzivre <sonyfauzi@outlook.co.id>
2022-07-08 07:42:31 +0700

    refactor: responsive home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,6 @@ import Image from 'next/image'
 import Layout from 'components/Layout'
 import Head from 'next/head'
 
-const Home: NextPage = () => {
-  return (
-    <Layout>
-      <Head>
-        <meta
-          name='description'
-          content='Muhammad Sony Fauzi Freelance Web developer, Penulis, Gamer'
-        />
-        <meta name='keywords' content='web, developer, freelance' />
-        <title>Arzivre</title>
-      </Head>
-      {Hero}
-      {Project1}
-      {Project2}
-    </Layout>
-  )
-}
-
-export default Home
-
 const Hero = (
   <article className='container mx-auto px-4 min-h-[90vh] mt-10 lg:flex gap-8'>
     <div>
@@ -35,14 +15,14 @@ const Hero = (
         blurDataURL='/images/profile.jpg'
       />
     </div>
-    <header className=''>
-      <h1 className='text-[calc(1em+6vh)] font-bold  mb-4'>
+    <section className='mb-4'>
+      <h1 className='text-[calc(1em+6vh)] font-bold mb-4'>
         Muhammad Sony Fauzi
       </h1>
       <h2 className='text-[calc(1em+5vh)] font-thin'>Web Developer,</h2>
       <h2 className='text-[calc(1em+5vh)] font-thin'>Penulis</h2>
-    </header>
-    <footer className='whitespace-nowrap px-4 text-right lg:text-left'>
+    </section>
+    <section className='whitespace-nowrap text-right lg:text-left'>
       <h3 className='text-2xl mb-1'>Sosial Media</h3>
       <p>
         <a
@@ -62,14 +42,13 @@ const Hero = (
           Github
         </a>
       </p>
-    </footer>
+    </section>
   </article>
 )
 
 const Project1 = (
-  <section className='container mx-auto px-4 min-h-screen my-20'>
-    <h1 className='text-[calc(1em+10vh)] mb-5'>Projects </h1>
-    <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-40'>
+  <section className='container mx-auto px-4 md:my-10'>
+    <div className='grid md:grid-cols-2 md:gap-1 lg:grid-cols-3 lg:gap-40'>
       <h2 className='text-4xl mb-4 md:mb-10 lg:mb-20'>Kopi Lokal</h2>
       <p className='mb-4 md:mb-10'>
         Kopi Lokal adalah project sampingan saya untuk menulis lokasi warung
@@ -80,7 +59,7 @@ const Project1 = (
       </p>
     </div>
     <div className='md:flex gap-4'>
-      <div className='text-[0px] border-4 border-gray-900'>
+      <div className='mb-4 text-[0px] border-4 border-gray-900'>
         <Image
           src='/images/kopi-lokal.png'
           alt='Project Kopi Lokal'
@@ -91,27 +70,32 @@ const Project1 = (
         />
       </div>
       <div className='w-80'>
-        <div className='mb-5'>
-          <h2 className='text-2xl mb-1'>URL</h2>
-          <a
-            target='_blank'
-            rel='noopener noreferrer'
-            href='https://kopilokal.com'
-            className='hover:text-blue-600'
-          >
-            <p>kopilokal.com</p>
-          </a>
-          <h2 className='text-2xl mb-1 mt-4'>Teknologi</h2>
-          <p>Nextjs</p>
-          <p>Supabase</p>
-          <p>MDX</p>
+        <div className='mb-5 flex justify-between gap-4 md:block'>
+          <div>
+            <h2 className='text-2xl mb-1'>URL</h2>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://kopilokal.com'
+              className='hover:text-blue-600'
+            >
+              kopilokal.com
+            </a>
+          </div>
+          <div>
+            <h2 className='text-2xl mb-1 mt-4'>Teknologi</h2>
+            <p>ReactJS</p>
+            <p>NextJS</p>
+            <p>Supabase</p>
+            <p>MDX</p>
+          </div>
         </div>
       </div>
     </div>
   </section>
 )
 const Project2 = (
-  <section className='container mx-auto px-4 min-h-screen my-10'>
+  <section className='container mx-auto px-4 md:my-10'>
     <div className='grid md:grid-cols-2 lg:grid-cols-3'>
       <h2 className='text-4xl mb-4 md:mb-10 lg:mb-20'>Houseofjesho</h2>
       <p className='mb-4 md:mb-10'>
@@ -120,7 +104,7 @@ const Project2 = (
       </p>
     </div>
     <div className='md:flex gap-4'>
-      <div className='text-[0px] border-4 border-gray-900'>
+      <div className='mb-4 text-[0px] border-4 border-gray-900'>
         <Image
           src='/images/house-of-jesho.png'
           alt='Project Kopi Lokal'
@@ -131,21 +115,47 @@ const Project2 = (
         />
       </div>
       <div className='w-80'>
-        <div className='mb-5'>
-          <h2 className='text-2xl mb-1'>URL</h2>
-          <a
-            href='https://houseofjesho.com'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='hover:text-blue-600'
-          >
-            <p>houseofjesho.com</p>
-          </a>
-          <h2 className='text-2xl mb-1 mt-4'>Teknologi</h2>
-          <p>Nextjs</p>
-          <p>Firebase</p>
+        <div className='mb-5 flex justify-between gap-4 md:block'>
+          <div>
+            <h2 className='text-2xl mb-1'>URL</h2>
+            <a
+              href='https://houseofjesho.com'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='hover:text-blue-600'
+            >
+              houseofjesho.com
+            </a>
+          </div>
+          <div>
+            <h2 className='text-2xl mb-1 md:mt-4'>Teknologi</h2>
+            <p>ReactJS</p>
+            <p>NextJS</p>
+            <p>Firebase</p>
+          </div>
         </div>
       </div>
     </div>
   </section>
 )
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <Head>
+        <meta
+          name='description'
+          content='Muhammad Sony Fauzi Freelance Web developer, Penulis, Gamer'
+        />
+        <meta name='keywords' content='web, developer, freelance' />
+        <title>Arzivre</title>
+      </Head>
+      {Hero}
+      <h2 className='relative md:left-20 px-4 mb-4 text-[calc(1em+8vh)]'>Projects </h2>
+      {Project2}
+      {Project1}
+    </Layout>
+  )
+}
+
+export default Home
