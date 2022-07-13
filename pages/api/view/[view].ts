f96a51b8afc4a49e36e5b2ee04891883ca018990
Arzivre <sonyfauzi@outlook.co.id>
2022-07-13 19:33:07 +0700

    refactor api

--- pages/api/view/[view].ts
+++ pages/api/view/[view].ts
@@ -11,14 +11,14 @@ export default async function handler(
     if (req.method === 'POST') {
       const newOrUpdatedViews = await prisma.views.upsert({
         where: { slug },
+        create: {
+          slug,
+          count: 1,
+        },
         update: {
           count: {
             increment: 1,
           },
-        },
-        create: {
-          slug,
-          count: 1,
         },
       })
 
